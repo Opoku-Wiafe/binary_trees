b6f9eee1870dcbf928ed72f242bc82b988872745
Opoku-Wiafe <opokuwiafevan@gmail.com>
2024-02-01 19:07:05 -0800

    final push

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_balance - measures the balance factor of a binary tree
+ * binary_tree_is_full - checks if a binary tree is full
  * @tree: pointer to the root node of the tree to measure the balance factor
  * Return: return the balance of the binary tree.
  */
