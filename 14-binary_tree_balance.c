9fcba05f69b2bb8fca167ed0523edb7f9118316a
Opoku-Wiafe <opokuwiafevan@gmail.com>
2024-02-01 17:57:20 -0800

    third update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,8 +10,23 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-
 	height_of_left = binary_tree_balance(tree->left);
 	height_of_right = binary_tree_balance(tree->right);
 	return (height_of_left - height_of_right);
+}
+/**
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree:  pointer to the root node of the tree to measure the height
+ * Return: return 0 if tree is NULL.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+	{
+		return (0);
+	}
+	size_t height_of_left = binary_tree_height(tree->left);
+	size_t height_of_right = binary_tree_height(tree->right);
+
+	return ((height_of_left > height_of_right ? height_of_left : height_of_right) + 1);
 }
