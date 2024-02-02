73e214eacf4532526827143fe68f7d7b635d1f37
Opoku-Wiafe <opokuwiafevan@gmail.com>
2024-02-01 19:03:46 -0800

    final push

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -4,13 +4,32 @@
  * @tree: pointer to the root node of the tree to measure the balance factor
  * Return: return the balance of the binary tree.
  */
-int binary_tree_balance(const binary_tree_t *tree)
+int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int height_of_left, height_of_right;
+	int checker = 1;
 
 	if (tree == NULL)
+	{
 		return (0);
-	height_of_left = binary_tree_balance(tree->left);
-	height_of_right = binary_tree_balance(tree->right);
-	return (height_of_left - height_of_right);
+	}
+	if ((tree->right && tree->left) || (!tree->right && !tree->left))
+	{
+		checker *= 1;
+	}
+	else
+	{
+		checker *= 0;
+	}
+
+	if (tree->right)
+	{
+		checker *= binary_tree_is_full(tree->right);
+	}
+
+	if (tree->left)
+	{
+		checker *= binary_tree_is_full(tree->left);
+	}
+
+	return (checker);
 }
