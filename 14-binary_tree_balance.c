532836f3534677f6484c280f7bc07b686100fe70
Opoku-Wiafe <opokuwiafevan@gmail.com>
2024-02-01 18:28:48 -0800

    make altrations

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+size_t binary_tree_height(const binary_tree_t *tree);
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance factor
@@ -6,14 +7,20 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int height_of_left, height_of_right;
+	int height_of_right, height_of_left, bal;
 
 	if (tree == NULL)
+	{
 		return (0);
-	height_of_left = binary_tree_balance(tree->left);
-	height_of_right = binary_tree_balance(tree->right);
-	return (height_of_left - height_of_right);
+	}
+
+	height_of_right = binary_tree_height(tree->right);
+	height_of_left = binary_tree_height(tree->left);
+	bal = height_of_left - height_of_right;
+
+	return (bal);
 }
+
 /**
  * binary_tree_height - function that measures the height of a binary tree
  * @tree:  pointer to the root node of the tree to measure the height
@@ -21,12 +28,18 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (!tree)
+	size_t height_of_right;
+	size_t height_of_left;
+
+	if (tree = NULL)
 	{
 		return (0);
 	}
-	size_t height_of_left = binary_tree_height(tree->left);
-	size_t height_of_right = binary_tree_height(tree->right);
-
-	return ((height_of_left > height_of_right ? height_of_left : height_of_right) + 1);
+	height_of_right = binary_tree_height(tree->right);
+	height_of_left = binary_tree_height(tree->left);
+	if (height_of_right > height_of_left)
+	{
+		return (height_of_right + 1);
+	}
+	return (height_of_left + 1);
 }
